eaeb823bc3148387f46de980a74b841154895801
Nicolae chelmenciuc <nicolaechelmenciuc@nicolaes-MacBook-Pro.local>
2020-03-06 09:22:39 +0000

    Adding the messages

---
+++ Hopper.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Hopper {
+
+    public static void main(String[] args) {
+        Board x = new Board();
+        Square s = new Square();
+        s
+
+    }
+}
---
+++ Square.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Square {
+
+    public Square() {
+        ImageIcon i = new ImageIcon("Water.png");
+        JButton b = new JButton(i);
+        b.setIcon(i);
+    }
+}
