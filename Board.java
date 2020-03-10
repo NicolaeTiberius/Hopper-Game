ec2e06e9d98ecc94e3b74907a5827691e0c04868
Nicolae chelmenciuc <nicolaechelmenciuc@nicolaes-MacBook-Pro.local>
2020-03-10 23:39:48 +0000

    Made the level board

--- Board.java
+++ Board.java
@@ -4,34 +4,46 @@ import javax.swing.JComponent;
 
 public class Board {
     private ImageIcon i = new ImageIcon("images/Water.png");
+    private ImageIcon l = new ImageIcon("images/LilyPad.png");
+    private ImageIcon gf = new ImageIcon("images/GreenFrog.png");
+    private ImageIcon rf = new ImageIcon("images/RedFrog.png");
     private Square[][]Sarray=new Square[5][5];
     private JFrame f = new JFrame();
-    //Square s = new Square(a, b);
-   // JPanel panel = new JPanel();
+
  
     
     public Board() {
-        //f.setContentPane(panel);
-       // panel.add(s);
-      //  f.add(panel);
+     
         GridLayout g = new GridLayout(5,5);
         f.setLayout(g);
         for(int j=0;j<5;j++){
             for(int b=0;b<5;b++){
                 Square s = new Square(j,b);
-                Sarray[j][b] = s;
+                Sarray[j][b] = s; 
                 f.add(Sarray[j][b].getButton());
+                //for printing the lillypads
+                if((j == 0 && b == 0)||(j == 0 && b ==2)||(j == 0 && b ==4)||(j == 2 && b ==0)|| (j == 2 && b ==4)||(j == 3 && b ==1)||(j == 3 && b ==3)){
+                    Sarray[j][b].imageReset(l);
+                }
+                //for printing the green frogs
+                else if((j == 1 && b == 1)||(j == 1 && b == 3)||(j == 2 && b == 2)||(j == 4 && b == 0)||(j == 4 && b == 4)){
+                    Sarray[j][b].imageReset(gf);
+                }
+                //for printing the red frogs
+                else if(j == 4&& b == 2){
+                    Sarray[j][b].imageReset(rf);
+                }
+
+                
             }
+        
         }
         f.setVisible(true);
         f.setTitle("Hoppers!");
-        f.setSize(700, 700);
+        f.setSize(750, 750);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        // for(i=0;i<25;i++){
-        //     a+=10;
-        //     b+=10;
-        // }
+      
     }
 
 }
--- Square.java
+++ Square.java
@@ -3,6 +3,13 @@ import java.awt.*;
 
 public class Square {
     private ImageIcon i = new ImageIcon("images/Water.png");
+    private ImageIcon rf1 = new ImageIcon("images/RedFrog.png");
+    private ImageIcon rf2 = new ImageIcon("images/RedFrog2.png");
+    private ImageIcon gf1 = new ImageIcon("images/GreenFrog.png");
+    private ImageIcon gf2 = new ImageIcon("images/GreenFrog2.png");
+    private ImageIcon l = new ImageIcon("images/LilyPad.png");
+
+    // private JButton bt = new JButton();
     private JButton b = new JButton();
     private int X_Pos = b.getLocation().x;
     private int Y_Pos = b.getLocation().y;
@@ -11,6 +18,16 @@ public class Square {
         xPos = X_Pos;
         yPos = Y_Pos;
         b.setIcon(i);
+        // bt.setIcon(l);
+
+    }
+
+    public void imageReset(ImageIcon a) {
+        b.setIcon(a);
+    }
+
+    public void moveTo(Square t) {
+
     }
 
     public int getLocx() {
@@ -24,4 +41,9 @@ public class Square {
     public JButton getButton() {
         return b;
     }
+
+    // public JButton typeButton() {
+    // return bt;
+    // }
+
 }
